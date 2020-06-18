cb0438f87ef88daba5c67707a6dc2f133e172391
Sanket <sanketkaware@gmail.com>
2020-06-17 18:48:33 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# React Drag and Drop
+
+## Installation
+
+Use the cmd line to install dependencies. 
+
+```
+npm install
+```
+
+## Usage
+
+```
+npm run build
+npm run dev
+```
---
+++ client/src/data/index.js
@@ -0,0 +1,46 @@
+const data = [{
+    id: 1,
+    icon: "❌",
+    status: "Reward",
+    title: "Reward 1",
+    content: "Reward 1"
+}, {
+    id: 2,
+    icon: "❌",
+    status: "Reward",
+    title: "Reward 2",
+    content: "Reward 2"
+}, {
+    id: 3,
+    icon: "❌",
+    status: "Reward",
+    title: "Reward 3",
+    content: "Reward 3"
+}, {
+    id: 4,
+    icon: "❌",
+    status: "Reward",
+    title: "Reward 4",
+    content: "Reward 4"
+}];
+
+const statuses = [{
+    status: "Reward",
+    icon: "❌",
+    color: "#EB5A46"
+}, {
+    status: "Category 1",
+    icon: "❌",
+    color: "#00C2E0"
+}, {
+    status: "Category 2",
+    icon: "❌",
+    color: "#C377E0"
+}, {
+    status: "Category 3",
+    icon: "❌",
+    color: "#3981DE"
+}];
+
+
+export { data, statuses };
